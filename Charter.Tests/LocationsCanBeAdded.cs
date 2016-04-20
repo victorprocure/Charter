6bb04279f693e14331c663de347f32c0ec0e37e8
Victor Procure <victor.procure@gmail.com>
2016-04-19 19:31:21 -0700

    Added new test

---
+++ Charter.Tests/LocationsCanBeAdded.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LocationsCanBeAdded.cs" company="Procure Development">
+//     Copyright (c) Procure Development. All rights reserved.
+// </copyright>
+// <author>Victor Procure</author>
+//-----------------------------------------------------------------------
+
+namespace Charter.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TestStack.BDDfy;
+
+    [TestClass]
+    [Story(
+        AsA = "As an administrator",
+        IWant = "I want to be able to add locations",
+        SoThat = "So that they can be used as arrivals or departures")]
+    internal class LocationsCanBeAdded
+    {
+    }
+}
